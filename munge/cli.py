278f9ce6ee9c2d1df228c4f3bfb30bc3c0a224c6
Thomas Anthony <anthony.p.tommy@gmail.com>
2015-08-31 15:03:31 -0700

    Updated converter.py to support different output methods for each type of output.
    This helps for outputting excel files where the output file
    should be opened in binary mode, whereas JSON and other file types
    should be opened as a text file. Additionally, I clarified the errors
    for when the the data type of the input file could not be automatically
    detected.
    
    I also added in the test-data directory, to provide examples in the
    README that can be replicated by the user. Updated requirements.txt
    to use more recent versions of packages.

--- munge/cli.py
+++ munge/cli.py
@@ -2,19 +2,9 @@ import click
 from munge import converter
 
 
-"""
-Current problems: csv, tsv importing
-TODO
-Add support for specifying import format
-Come up with a name for my tablib
-Add XML support
-Move the logic out of the CLI interface
-"""
-
-
 @click.command()
-@click.argument("data_type", type=click.Choice(converter.OUTPUT.keys()))
-@click.option('-i', '--in_file', type=click.File('rb'), default="-")
-@click.option('-o', '--out_file', type=click.File('wb'), default="-")
+@click.argument('data_type', type=click.Choice(converter.OUTPUT.keys()))
+@click.option('-i', '--in_file', type=click.File('rb'), default='-')
+@click.option('-o', '--out_file', type=click.File('wb'), default='-')
 def convert(data_type, in_file, out_file):
     converter.convert(data_type, in_file, out_file)
--- munge/converter.py
+++ munge/converter.py
@@ -1,40 +1,61 @@
 import tablib
 from io import TextIOWrapper
+from tablib.formats import available
+
+SUPPORTED = [format.title for format in available]
+INPUT_FORMATS = {
+    format.title: format.import_set
+    for format in available if hasattr(format, "import_set")
+}
+INPUT_TYPES = INPUT_FORMATS.keys()
 
 
 def read_bytes(in_file):
     return in_file.read()
 
 
-def read_text(in_file):
-    return in_file.read().decode()
-
-
-def write_text(output, text, end=""):
+def write_text(output, text, end=''):
     with TextIOWrapper(output) as out:
         print(text, file=out, end=end)
 
-# json library doesn't append a newline at the end, so we will
-write_json = lambda output, text: write_text(output, text, "\n")
+# json and html library don't append a newline at the end
+write_string = lambda output, text: write_text(output, text, '\n')
 
 
 def write_bytes(output, data):
     output.write(data)
 
 # Method to output the data for each type
-OUTPUT = {"json": write_json,
-          "html": write_text,
-          "yaml": write_text,
-          "csv":  write_text,
-          "tsv":  write_text,
-          "xls":  write_bytes,
-          "xlsx": write_bytes}
+OUTPUT = {'json': write_string,
+          'html': write_string,
+          'yaml': write_text,
+          'csv':  write_text,
+          'tsv':  write_text,
+          'xls':  write_bytes,
+          'xlsx': write_bytes}
+
+
+READER_TYPES = [lambda data: data,  # change nothing, read as binary data
+                lambda data: data.decode()]  # decode data for txt documents
 
 
 def convert(data_type, in_file, out_file):
     data = read_bytes(in_file)
-    data_set = tablib.import_set(data)
-    writer = OUTPUT[data_type]
+    for reader in READER_TYPES:
+        # try each reader until we find a way to import data
+        data_set = tablib.import_set(reader(data))
+        if data_set:
+            break
 
-    # dynamically get the data type we want. ex: getattr(data_set, "html")
-    writer(out_file, getattr(data_set, data_type))
+    if data_set:
+        # get the correct function to write the output data
+        writer = OUTPUT[data_type]
+
+        # dynamically get the data type we want. ex: getattr(data_set, 'html')
+        writer(out_file, getattr(data_set, data_type))
+    else:
+        raise TypeError(
+            "Could not detect input data type.\n"
+            "Ensure the data provided from '%s' is of type: %s."
+            % (in_file.name, ', '.join(INPUT_TYPES))
+        )
